1e0c5601153dddc7d3ced7249f733349bfec4619
Prateek Sarangi <sarangiprateek80@gmail.com>
2019-06-22 12:18:28 +0530

    Dropout test success!!

--- func/nets/vid_enc.py
+++ func/nets/vid_enc.py
@@ -2,7 +2,6 @@ import chainer.links as L
 import chainer.functions as F
 from chainer import cuda, serializers, Chain, Variable
 import chainer
-import torch.nn as nn
 from os.path import dirname
 import json
 import numpy as np
@@ -13,17 +12,13 @@ import sys
 
 class Model(Chain):
 
-    ########################################################################
-
-    def __init__(self, b_size={'video': 5}):
+    def __init__(self, b_size = {'video':5}):
         self.b_size = b_size
 
         super(Model, self).__init__(
-            conv2=L.ConvolutionND(3, 1, 20, 5),
-            conv3=L.ConvolutionND(3, 20, 20, 5),
-            fc1=L.Linear(4096, 1300),
-            fc4=L.Linear(1300,1300),
-            fc5=L.Linear(1300, 300),
+            fc_v1=L.Linear(4096, 1000),
+            fc_v2=L.Linear(1000, 1000),
+            fc_v3=L.Linear(1000, 300),
         )
 
     def __call__(self, x_seg):
@@ -32,12 +27,8 @@ class Model(Chain):
         '''
         b_size = self.b_size['video']
         with cuda.get_device(x_seg.data):
-            y0 = F.tanh(self.fc1(x_seg))
-            y1 = F.tanh(self.fc4(y0))
-            y2 = F.tanh(self.fc5(y1))
-            h = F.dropout(F.relu(self.fc1(y2)))
-            h = self.fc4(h)
-            h = F.reshape(h, (int(h.shape[0] / b_size), b_size, 300))
+            y0 = F.tanh(self.fc_v1(x_seg))
+            y1 = F.tanh(self.fc_v2(y0))
+            y2 = F.dropout(F.relu(self.fc_v3(y1)))
+            h = F.reshape(y2, (int(y2.shape[0] / b_size), b_size, 300))
             return F.sum(h, axis=1) / b_size
-
-    ##########################################################################
