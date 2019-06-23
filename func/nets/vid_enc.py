84bb6e1b8adc432084f6a634e4195b9e1ea4723f
Prateek Sarangi <sarangiprateek80@gmail.com>
2019-06-23 20:19:00 +0530

    Convolution dimension not successful to debug. Further improvement in dimension required.

--- func/nets/vid_enc.py
+++ func/nets/vid_enc.py
@@ -11,16 +11,12 @@ import sys
 
 
 class Model(Chain):
-
-    def __init__(self, b_size = {'video':5}):
+    def __init__(self, b_size={'video': 5}):
         self.b_size = b_size
-        print(b_size['video'])
+
         super(Model, self).__init__(
-            l1 = L.ConvolutionND(2, 1, 20, 5),    #L.Convolution(ndim, in_channel, out_chennel, stride)
-            l2 = L.ConvolutionND(2, 20, 20, 5),
-            l3 = L.Linear(2500, 1000),
-            l4 = L.Linear(1000, 1000),
-            l5 = L.Linear(1000, 300),
+            fc_v1=L.Linear(4096, 1000),
+            fc_v2=L.Linear(1000, 300),
         )
 
     def __call__(self, x_seg):
@@ -29,11 +25,7 @@ class Model(Chain):
         '''
         b_size = self.b_size['video']
         with cuda.get_device(x_seg.data):
-            print(x_seg.shape)
-            y0 = F.max_pooling_nd(F.local_response_normalization(F.relu(self.l1(x_seg))), 3, stride=2)
-            y1 = F.max_pooling_nd(F.local_response_normalization(F.relu(self.l2(y0))), 3, stride=2)
-            y2 = F.tanh(self.l3(y1))
-            y3 = F.dropout(F.relu(self.l4(y2)))
-            y4 = F.tanh(self.l5(y3))
-            h = F.reshape(y4, (int(y4.shape[0] / b_size), b_size, 300))
+            y0 = F.tanh(self.fc_v1(x_seg))
+            y1 = F.dropout(F.relu(self.fc_v2(y0)))
+            h = F.reshape(y1, (int(y1.shape[0] / b_size), b_size, 300))
             return F.sum(h, axis=1) / b_size
